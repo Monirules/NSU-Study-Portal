b6faf2e1733ca59d0779d3b8c90ca9559bcbb3f5
Monirul Islam <monirul.mahmud@northsouth.edu>
2022-12-31 18:26:40 +0600

    Updated models.py

--- dashboard/models.py
+++ dashboard/models.py
@@ -19,6 +19,25 @@ class Notes(models.Model):
          return self.title
 
 
+class Complain(models.Model):
+         email = models.EmailField(max_length=100)
+         complain = models.CharField(max_length=200)
+         against = models.CharField(max_length=200)
+         position = models.CharField(max_length=200)
+         image =  models.ImageField(upload_to = 'static', null=True, blank=True, default='2.png')
+         def __str__(self) :
+             return self.email 
+
+
+class Comment(models.Model):
+    email = models.EmailField(max_length=100)
+    complain = models.ForeignKey(Complain, related_name="comments", on_delete=models.CASCADE)
+    username = models.CharField(max_length=80)
+    body = models.TextField(max_length=190)
+    def __str__(self) :
+        return self.complain.email
+    
+    
 class Homework(models.Model):
     user=models.ForeignKey(User,on_delete=models.CASCADE)
     subject=models.CharField(max_length=50)
